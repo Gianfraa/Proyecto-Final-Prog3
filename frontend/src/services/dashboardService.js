545972a13326965c94d6d1d8d8860c5ed4a4587e
NaxarenoXNT <nazarenomoretti12@gmail.com>
2026-07-11 16:37:29 -0300

    fix: arregle las rutas de dashboard

--- frontend/src/services/dashboardService.js
+++ frontend/src/services/dashboardService.js
@@ -1,16 +1,16 @@
 import api from './api';
 
 export const getBalance = async () => {
-  const response = await api.get('/dashboard/balance');
+  const response = await api.get('/balance');
   return response.data; // { balance, totalIngresos, totalGastos }
 };
 
 export const getResumen = async (mes) => {
-  const response = await api.get('/dashboard/resumen', { params: { mes } });
+  const response = await api.get('/resumen', { params: { mes } });
   return response.data; // { mes, totalIngresos, totalGastos, balance, cantidadTransacciones, gastosPorCategoria }
 };
 
 export const getEstadisticas = async () => {
-  const response = await api.get('/dashboard/estadisticas');
+  const response = await api.get('/estadisticas');
   return response.data; // { totalTransacciones, totalIngresos, totalGastos, balance, promedioGasto, categoriaTopGasto, gastosPorCategoria, evolucionMensual }
 };
